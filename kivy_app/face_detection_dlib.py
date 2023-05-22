5697558d8b8296f3c3e58baa36316f91483526f8
mattisd2603 <mattisd2603@gmail.com>
2023-05-22 19:16:57 +0200

    Auslagerung DetectionScreen Funktion

---
+++ kivy_app/face_detection_dlib.py
@@ -0,0 +1,97 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+import cv2
+import threading
+import dlib
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy.uix.image import Image
+from imutils import face_utils
+import features as feat
+
+class DetectionScreen(Screen):
+    def initialize(self):
+        layout = BoxLayout(orientation='vertical')
+
+        button_to_main = Button(text='Go to Main')
+        button_to_main.bind(on_press=lambda x: self.set_screen('main'))
+        layout.add_widget(button_to_main)
+
+        self.image = Image()
+        layout.add_widget(self.image)
+
+        self.add_widget(layout)
+
+        #Starting the video in a new Thread
+        threading.Thread(target=self.initialize_resources).start()
+
+        Clock.schedule_interval(self.update, 0.05)
+
+    def initialize_resources(self):
+        self.capture = cv2.VideoCapture(0)
+  
+        # loading the dlib face detector
+        self.detector = dlib.get_frontal_face_detector()
+        # Path to the shape predictor model
+        self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+    def update(self, dt):            
+        
+        # Variables
+        blink_thresh = 0.45
+        count_frame = 0
+        
+        # Eye landmarks
+        (L_start, L_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+        (R_start, R_end) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
+
+        if hasattr(self, 'capture'):
+            ret, frame = self.capture.read()
+            if ret:
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = self.detector(gray)
+                for rect in faces:
+                    # Zeichne ein Rechteck um das erkannte Gesicht
+                    x1, y1, x2, y2 = rect.left(), rect.top(), rect.right(), rect.bottom()  # noqa: E501
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+                    # Erhalte die Landmarks für das Gesicht und zeichne sie
+                    shape = self.predictor(gray, rect)
+                    for i in range(shape.num_parts):
+                        p = shape.part(i)
+                        cv2.circle(frame, (p.x, p.y), 2, (0, 255, 0), -1)
+                    
+                    # converting the shape class directly
+                    # to a list of (x,y) coordinates
+                    shape = face_utils.shape_to_np(shape)
+        
+                    # parsing the landmarks list to extract
+                    # lefteye and righteye landmarks--#
+                    lefteye = shape[L_start: L_end]
+                    righteye = shape[R_start:R_end]
+        
+                    # Calculate the EAR
+                    left_EAR = feat.calculate_EAR(lefteye)
+                    right_EAR = feat.calculate_EAR(righteye)
+        
+                    # Avg of left and right eye EAR
+                    avg = (left_EAR+right_EAR)/2
+                    if avg < blink_thresh:
+                        count_frame += 1  # incrementing the frame count
+                    else:
+                        cv2.putText(frame, 'Blink Detected', (30, 30),
+                                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1)
+                        count_frame = 0
+        
+                # Konvertiere das Bild in ein Format, das von Kivy verwendet werden kann
+                buf1 = cv2.flip(frame, 0)
+                buf = buf1.tostring()
+                image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), 
+                                               colorfmt='bgr')
+                image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+
+                self.image.texture = image_texture
+
+    def set_screen(self, screen_name):
+        self.manager.current = screen_name
--- kivy_app/main.py
+++ kivy_app/main.py
@@ -1,23 +1,13 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.button import Button
-from kivy.uix.camera import Camera
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.progressbar import ProgressBar
 from kivy.uix.label import Label
 from kivy.uix.switch import Switch
-import cv2
-import threading
 import os
-import dlib
-import time
-import math
 from kivy.clock import Clock
-from kivy.graphics.texture import Texture
-from kivy.uix.image import Image
-from scipy.spatial import distance as dist
-import numpy as np
-from imutils import face_utils
+from face_detection_dlib import DetectionScreen
 
 os.environ['KIVY_CAMERA'] = 'opencv'
 
@@ -60,7 +50,8 @@ class LoadingScreen(Screen):
         layout.add_widget(self.progress_bar)
 
         self.progress_value = 0
-        self.progress_update_event = Clock.schedule_interval(self.update_progress_bar, 0.05)
+        self.progress_update_event = Clock.schedule_interval(
+                                     self.update_progress_bar, 0.05)
 
         self.add_widget(layout)
 
@@ -74,107 +65,7 @@ class LoadingScreen(Screen):
         else:
             self.progress_value = 0
             self.progress_bar.value = self.progress_value
-
-class DetectionScreen(Screen):
-    def initialize(self):
-        layout = BoxLayout(orientation='vertical')
-
-        button_to_main = Button(text='Go to Main')
-        button_to_main.bind(on_press=lambda x: self.set_screen('main'))
-        layout.add_widget(button_to_main)
-
-        self.image = Image()
-        layout.add_widget(self.image)
-
-        self.add_widget(layout)
-
-        # Starte das Laden der Modelle und das Starten der Videoaufnahme in einem separaten Thread
-        threading.Thread(target=self.initialize_resources).start()
-
-        Clock.schedule_interval(self.update, 0.05)
-
-    def initialize_resources(self):
-        self.capture = cv2.VideoCapture(0)
-  
-        # Lade den dlib Detektor und den Shape Predictor
-        self.detector = dlib.get_frontal_face_detector()
-        # Path to the shape predictor model
-        self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")  
-
-    def update(self, dt):
-
-        # defining a function to calculate the EAR
-        def calculate_EAR(eye):
-        
-            # calculate the vertical distances
-            y1 = dist.euclidean(eye[1], eye[5])
-            y2 = dist.euclidean(eye[2], eye[4])
-        
-            # calculate the horizontal distance
-            x1 = dist.euclidean(eye[0], eye[3])
-        
-            # calculate the EAR
-            EAR = (y1+y2) / x1
-            return EAR
-        
-        # Variables
-        blink_thresh = 0.45
-        count_frame = 0
-        
-        # Eye landmarks
-        (L_start, L_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-        (R_start, R_end) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
-
-        if hasattr(self, 'capture'):
-            ret, frame = self.capture.read()
-            if ret:
-                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-                faces = self.detector(gray)
-                for rect in faces:
-                    # Zeichne ein Rechteck um das erkannte Gesicht
-                    x1, y1, x2, y2 = rect.left(), rect.top(), rect.right(), rect.bottom()
-                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-
-                    # Erhalte die Landmarks für das Gesicht und zeichne sie
-                    shape = self.predictor(gray, rect)
-                    for i in range(shape.num_parts):
-                        p = shape.part(i)
-                        cv2.circle(frame, (p.x, p.y), 2, (0, 255, 0), -1)
-                    
-                    # converting the shape class directly
-                    # to a list of (x,y) coordinates
-                    shape = face_utils.shape_to_np(shape)
-        
-                    # parsing the landmarks list to extract
-                    # lefteye and righteye landmarks--#
-                    lefteye = shape[L_start: L_end]
-                    righteye = shape[R_start:R_end]
-        
-                    # Calculate the EAR
-                    left_EAR = calculate_EAR(lefteye)
-                    right_EAR = calculate_EAR(righteye)
-        
-                    # Avg of left and right eye EAR
-                    avg = (left_EAR+right_EAR)/2
-                    # Avg of left and right eye EAR
-                    if avg < blink_thresh:
-                        count_frame += 1  # incrementing the frame count
-                    else:
-                        cv2.putText(frame, 'Blink Detected', (30, 30),
-                                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1)
-                        count_frame = 0
-        
-                # Konvertiere das Bild in ein Format, das von Kivy verwendet werden kann
-                buf1 = cv2.flip(frame, 0)
-                buf = buf1.tostring()
-                image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-                image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-
-                self.image.texture = image_texture
-
-    def set_screen(self, screen_name):
-        self.manager.current = screen_name
-
+            
 class SettingsScreen(Screen):
     def __init__(self, **kwargs):
         super(SettingsScreen, self).__init__(**kwargs)
