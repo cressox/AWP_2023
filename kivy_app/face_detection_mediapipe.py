b6c55fac67f490d83444b88aabf1619d2f290021
mattisd2603 <mattisd2603@gmail.com>
2023-06-23 18:57:17 +0200

    Docstrings und Visuelle Statusanzeige

--- kivy_app/face_detection_mediapipe.py
+++ kivy_app/face_detection_mediapipe.py
@@ -12,102 +12,185 @@ from scipy.spatial import distance as dist
 
 class DetectionScreen(Screen):
     def initialize(self):
+        """
+        Initializes the application by scheduling the initialization of resources.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         Clock.schedule_once(self.initialize_resources)
 
-    def initialize_resources(self,n):
+    def initialize_resources(self, n):
+        """
+        Initializes the necessary resources for the application.
+
+        Parameters:
+            n (int): The parameter n.
+
+        Returns:
+            None
+
+        """
+
+        # Initialize the Image object
         self.image = Image()
 
+        # Schedule the update function to be called every 0.02 seconds
         Clock.schedule_interval(self.update, 0.02)
 
+        # Initialize variables
         self.fps = 0
         self.update_event = None
         self.draw_landmarks = True
 
-        # Loading the important packages from Mediapipe
-
+        # Importing the required packages from Mediapipe
         self.mp_drawing = mp.solutions.drawing_utils
         self.mp_face_mesh = mp.solutions.face_mesh
         self.mp_drawing_styles = mp.solutions.drawing_styles
         self.denormalize_coordinates = self.mp_drawing._normalized_to_pixel_coordinates
-        
-        # Defining the Face Mesh
+
+        # Define the Face Mesh object
         self.face_mesh = self.mp_face_mesh.FaceMesh(
-                    static_image_mode=False,         # Default=False
-                    max_num_faces=1,                # Default=1
-                    # Unlike the default mode, this gives us the landmarks of the 
-                    # iris and eyes in addition to the facial landmarks (478 in total)
-                    refine_landmarks=True,         # Default=False
-                    min_detection_confidence=0.5,   # Default=0.5
-                    min_tracking_confidence= 0.5,)
-        
-        # Initialisation of values for blink detection
+            static_image_mode=False,         # Default=False
+            max_num_faces=1,                 # Default=1
+            refine_landmarks=True,           # Default=False
+            min_detection_confidence=0.5,    # Default=0.5
+            min_tracking_confidence=0.5,     # Default=0.5
+        )
+
+        # Initialize values for blink detection
         self.count_frame = 0
         self.blink_thresh = 0.16
         self.succ_frame = 1
 
+        # Initialize the Counting length for repetition of warning sound
         self.count_warning_frame_eyes_closed = 20
-
         self.count_warning_frame_classifier = 100
 
-        # counter for capturing movement
+        # Counter for capturing movement
         self.movement_counter = 0
 
-        # Initialisation of list of frames for calculation of PERCLOS
+        # Lists to store eye closure, EAR, blink durations, and predictions
         self.list_of_eye_closure = []
-
-        # Initialisation of list of frames for calculation of blink threshold
         self.list_of_EAR = []
-
         self.list_of_blink_durations = []
-        self.list_of_blink_frequency = []
+        self.list_of_predictions = []
 
+        # Initialize values for awake state
         self.awake_ear_eyes_open = 0
         self.awake_perclos = 0.01
         self.awake_blink_duration = 0
         self.awake_avg_ear = 0
 
+        # Flags to control calculations
         self.cal_perclos = False
         self.cal_ear = False
         self.cal_blinks = False
 
         self.count_last = -1
         self.cal_done = False
-
+        
+        # Blink Counter
         self.blinks = 0
 
-        # Select the 6 landmarks per eye 
-        # for the calculation of the eye aspect ratio
-        # The chosen 12 points:   P1,  P2,  P3,  P4,  P5,  P6
-        self.left_eye_idxs  =    [362, 385, 387, 263, 373, 380]
-        self.right_eye_idxs =    [33,  160, 158, 133, 153, 144]
+        # Predicition, Initialize with 0 = Awake
+        self.median_prediction = 0
+
+        # Indices of landmarks for left and right eyes
+        self.left_eye_idxs = [362, 385, 387, 263, 373, 380]
+        self.right_eye_idxs = [33, 160, 158, 133, 153, 144]
 
         self.eye_idxs = self.left_eye_idxs + self.right_eye_idxs
 
         Logger.info("Mediapipe: 478 Landmarks are detected")
 
     def on_enter(self):
+        """
+        Event handler called when entering the screen.
+
+        It starts the camera by calling the `start_camera` function.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         self.start_camera()
 
     def on_leave(self):
+        """
+        Event handler called when leaving the screen.
+
+        It stops the camera by calling the `stop_camera` function.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         self.stop_camera()
 
     def start_camera(self):
+        """
+        Starts the camera capture.
+
+        It initializes the video capture using `cv2.VideoCapture`.
+        The frames per second (fps) is obtained from the capture.
+        An update event is scheduled using `Clock.schedule_interval` to call the `update` function.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         self.capture = cv2.VideoCapture(0)
         self.fps = self.capture.get(cv2.CAP_PROP_FPS)
         self.update_event = Clock.schedule_interval(self.update, 1/self.fps)
-        print(self.fps)
 
     def stop_camera(self):
+        """
+        Stops the camera capture.
+
+        It releases the video capture using `release` and cancels the update event using `Clock.unschedule`.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
         if self.capture is not None:
             self.capture.release()
             self.capture = None
         if self.update_event is not None:
             Clock.unschedule(self.update_event)
             self.update_event = None
-        self.blinks = 0
 
     def update(self, dt):
-        
+        """
+        Update function called at regular intervals to process frames from the camera.
+
+        It performs the following tasks:
+        - Reads a frame from the video capture.
+        - Converts the frame to RGB color format for processing with Mediapipe.
+        - Generates the face mesh using Mediapipe.
+        - Processes the landmarks and performs blink detection.
+        - Calculates PERCLOS, average EAR, and other metrics.
+        - Detects the Status: Awake/Questionable/Tired
+        - Displays visual indicators and text based on the detection results.
+
+        Parameters:
+            dt (float): The time interval since the last update in seconds.
+
+        Returns:
+            None
+        """        
         if hasattr(self, 'capture') and hasattr(self, 'fps') and hasattr(self, 'face_mesh') and self.manager.current == 'detection':
             # Read a frame from the video capture
             ret, frame = self.capture.read()
@@ -146,7 +229,7 @@ class DetectionScreen(Screen):
                             
                             self.count_last +=1
 
-                            #Calculating the Eye Aspect ratio for the left and right eye
+                            # Calculating the Eye Aspect ratio for the left and right eye
                             EAR_left = self.calculate_EAR(coord_points_left)
                             
                             EAR_right = self.calculate_EAR(coord_points_right)
@@ -157,11 +240,10 @@ class DetectionScreen(Screen):
                             # Blink Detection Algorithm
                             blink, closed_eye, blink_duration = self.blink_detection(avg_EAR)
 
+                            # Defining the length for the Lists of the features
                             frame_length_perclos = 1000
                             frame_length_ear_list = 1000
                             num_of_blinks = 25
-
-
 
                             # PERCLOS Calculation based on frames
                             perclos = self.calculate_perclos(closed_eye, 
@@ -193,11 +275,6 @@ class DetectionScreen(Screen):
 
                             # When the calibration is done
                             if self.cal_done:
-                                # Putting the PERCLOS value on Screen
-                                perclos_text = round(perclos, 2)
-                                string_perclos = "PERCLOS: " + str(perclos_text)
-                                cv2.putText(image, string_perclos, (30, 120),
-                                cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1)
 
                                 # Generation of the feature vector
                                 feature_vector = self.feature_vector(perclos, 
@@ -207,17 +284,34 @@ class DetectionScreen(Screen):
 
                                 # Prediction of the feature vector whether 
                                 # tired/half-tired/awake
-                                prediction = self.new_input(feature_vector)
+                                single_prediction = self.new_input(feature_vector)
+                                length = 250
 
-                                print(prediction)
-
-                                if prediction == 0:
-                                    pass
+                                # Median Prediction
+                                pred = self.prediction_median(single_prediction, length)
+                                if isinstance(pred, list):
+                                    pred = pred[0]
+                                
+                                pred = int(pred)
+                                
+                                # Visual and auditive apperance depending on prediction
+                                if pred == 0:
+                                    # Putting the Prediction value on Screen
+                                    string_perclos = "Wach"
+                                    cv2.putText(image, string_perclos, (30, 120),
+                                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1)
                                     #TODO Visual apperance
-                                elif prediction == 1:
-                                    pass
+                                elif pred == 1:
+                                    # Putting the Prediction value on Screen
+                                    string_perclos = "Fraglich"
+                                    cv2.putText(image, string_perclos, (30, 120),
+                                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1)
                                     #TODO Visual apperance
                                 else:
+                                    # Putting the Prediction value on Screen
+                                    string_perclos = "Müde"
+                                    cv2.putText(image, string_perclos, (30, 120),
+                                    cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1)
                                     if self.count_warning_frame_classifier == 100:
                                         self.count_warning_frame_classifier = 0
                                         self.play_warning_sound()
@@ -238,7 +332,7 @@ class DetectionScreen(Screen):
                                 cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1)
 
                 else:
-                    # if unable to detect landmarks for 100 frames,
+                    # If unable to detect landmarks for 100 frames,
                     # then give warning signs
                     self.movement_counter += 1
                     if self.movement_counter == 100:
@@ -260,15 +354,25 @@ class DetectionScreen(Screen):
 
         This method loads and plays a warning sound from the 'warning.ogg' file. 
         """
-        sound = SoundLoader.load('assets\warning.ogg')
+        sound = SoundLoader.load('assets/warning.ogg')
         if sound:
             sound.play()
 
     def set_screen(self, screen_name):
+        """
+        Set the current screen to the specified screen name.
+
+        Parameters:
+        - screen_name: The name of the target screen to switch to.
+
+        Returns:
+        None
+        """
         self.manager.current = screen_name 
 
     def blink_detection(self, avg_EAR: float):
-        """Calculates the blink behavior based on the EAR value. 
+        """
+        Calculates the blink behavior based on the EAR value. 
         If the threshold (how far the eyes are closed)
         is not reached , blinking is detected.
 
@@ -311,7 +415,8 @@ class DetectionScreen(Screen):
         return blink, eye_closed, blink_duration
 
     def calculate_perclos(self, closed_eye: bool, length_of_frames: int):
-        """Calculates the PERCLOS (percentage of eye closure) value 
+        """
+        Calculates the PERCLOS (percentage of eye closure) value 
         based on the number of frames the eye is closed
 
         Args:
@@ -351,7 +456,8 @@ class DetectionScreen(Screen):
         return perclos
     
     def get_coord_points(self, landmark_list: list, eye_idxs: list, imgW: int, imgH: int):
-        """Function for getting all six coordinate points of one eye
+        """
+        Function for getting all six coordinate points of one eye
 
         Parameters:
             landmark_list (list): a list of all landmarks from the mediapipe face mesh
@@ -377,7 +483,8 @@ class DetectionScreen(Screen):
         return coords_points
 
     def calculate_EAR(self, eye: list):
-        """Function for calculating the EAR (Eye Aspect Ratio)
+        """
+        Function for calculating the EAR (Eye Aspect Ratio)
 
         Parameters:
             eye (list): 6-entry large array of the coordinate points (x, y)
@@ -407,7 +514,8 @@ class DetectionScreen(Screen):
         return EAR
     
     def get_list_of_ear(self, avg_ear: float, length: int):
-        """For a given length of frames a list ist build 
+        """
+        For a given length of frames a list ist build 
         with the EAR for the last frames
 
         Args:
@@ -432,7 +540,8 @@ class DetectionScreen(Screen):
             pass
     
     def avg_ear_eyes_open(self):
-        """Returns us over the length of the specified lists 
+        """
+        Returns us over the length of the specified lists 
         at values where the eye is open is the mean
 
         Returns
@@ -459,10 +568,26 @@ class DetectionScreen(Screen):
         return avg_ear_eyes_open
      
     def avg_ear(self):
+        """
+        Calculate and return the average Eye Aspect Ratio (EAR) from the list of EAR values.
+
+        Returns:
+        - The average EAR value as a float.
+
+        """
         return np.mean(self.list_of_EAR)
     
     def avg_blink_duration(self, frame_blink_duration, length):
+        """
+        Calculate and return the average blink duration based on the given frame blink duration.
 
+        Args:
+        - frame_blink_duration (float): The duration of the current blink frame.
+        - length (int): The desired length of the time span in frames for calculating the average.
+
+        Returns:
+        - The average blink duration as a float.
+        """
         number_of_frames = len(self.list_of_blink_durations)
 
         avg_blink_duration = 1
@@ -527,50 +652,82 @@ class DetectionScreen(Screen):
         # Checking if the frame length of PERCLOS and EAR is done
         if self.cal_ear and self.cal_perclos and self.cal_blinks:
             self.cal_done = True
+        elif self.cal_ear and self.cal_perclos and not self.cal_blinks:
+            calibrate_status = self.blinks/num_of_blinks
 
         return calibrate_status
     
     def feature_vector(self, frame_perclos, frame_blink_duration, frame_avg_ear_eyes_open, frame_avg_ear):
         """
-        Calculate the feature vector based on the difference between 
-        the frame PERCLOS and the awake PERCLOS.
+        Calculate the feature vector based on the ratios of PERCLOS, Average EAR for eyes open, Average EAR and Blink duration
 
         Args:
             frame_perclos (float): PERCLOS value for the current frame.
+            frame_blink_duration (float): Blink duration for the current frame.
+            frame_avg_ear_eyes_open (float): Average EAR for eyes open for the current frame.
+            frame_avg_ear (float): Average EAR for the current frame.
 
         Returns:
-            list: Feature vector containing the difference 
-            between frame PERCLOS and awake PERCLOS.
+            list: Feature vector containing the ratios between the frame and awake values of PERCLOS, blink duration, average EAR for eyes open, and average EAR.
 
         """
+        
+        # Calculate the Ratios
         ratio_avg_ear = frame_avg_ear/self.awake_avg_ear
         ratio_avg_ear_eyes_open = frame_avg_ear_eyes_open/self.awake_ear_eyes_open
         ratio_blink_duration = frame_blink_duration/self.awake_blink_duration
         ratio_perclos = frame_perclos/self.awake_perclos
-        feature_vector = [ratio_perclos, ratio_blink_duration, ratio_avg_ear_eyes_open, ratio_avg_ear]
 
-        feature_vector = [ratio_perclos]
-        return feature_vector
+        return [ratio_perclos, ratio_blink_duration, ratio_avg_ear_eyes_open, ratio_avg_ear]
     
     def new_input(self, feature_vector):
         """
-        Perform prediction using a loaded classifier based on the given feature vector.
+        Make a prediction based on the given feature vector using the pre-trained classifier.
 
         Args:
-            feature_vector (list): Feature vector for the input.
+            feature_vector (list): Feature vector containing the ratios of PERCLOS, blink duration, average EAR for eyes open, and average EAR.
 
         Returns:
-            str: Prediction result.
+            int: Prediction indicating the state of drowsiness (0: awake, 1: half-tired, 2: tired).
 
         """
-
-        # Load the Classifier
-        loaded_classifier = joblib.load("best_classifier.pkl")
-
-        # Predict the Class of the Feature Vector
-        prediction = loaded_classifier.predict([feature_vector])
+        classifier_all  = joblib.load("best_classifier.pkl")
+        
+        if feature_vector[0] < 1.0:
+            prediction = 0
+        else:
+            prediction = classifier_all.predict([feature_vector])
+            if isinstance(prediction, list):
+                prediction = prediction[0]
 
         return prediction
+    
+    def prediction_median(self, prediction, length):
+        """
+        Calculate the median prediction based on a given prediction value.
+
+        Args:
+            prediction (int): Prediction value indicating the state of drowsiness (0: awake, 1: half-tired, 2: tired).
+            length (int): Length of the time span (in frames) for calculating the median.
+
+        Returns:
+            float: Median prediction value.
+
+        """
+        number_of_frames = len(self.list_of_predictions)
+        self.median_prediction = 0
+
+        # Calculation when time span has been reached
+        if number_of_frames == length:
+            self.list_of_predictions.append(prediction)
+            self.list_of_predictions.pop(0)
+            self.median_prediction = np.median(self.list_of_predictions)
+            
+        # Collect predictions until time span (in frames) has been reached
+        elif number_of_frames < length:
+            self.list_of_predictions.append(prediction)
+        
+        return self.median_prediction  
     
 
     def yawning():
